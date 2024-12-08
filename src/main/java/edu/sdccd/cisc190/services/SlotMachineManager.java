36a0a91ed418a261cbddb9f195a7f6b4ad5159bf
WaseeIbrahim07 <166268421+WaseeIbrahim07@users.noreply.github.com>
2024-12-07 19:42:19 -0800

    Update SlotMachineManager.java

--- src/main/java/edu/sdccd/cisc190/services/SlotMachineManager.java
+++ src/main/java/edu/sdccd/cisc190/services/SlotMachineManager.java
@@ -1,5 +1,3 @@
-package edu.sdccd.cisc190.services;
-
 import edu.sdccd.cisc190.machines.*;
 import edu.sdccd.cisc190.players.bots.*;
 import org.slf4j.Logger;
@@ -12,28 +10,28 @@ import java.util.List;
  * Manages slot machines and bots interacting with them.
  * Handles the creation, assignment, rotation and control of bot threads interacting with different slot machines
  * */
+
 public class SlotMachineManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(SlotMachineManager.class);
 
     // Instances of slot machines
-    static DiamondDash diamondDash = new DiamondDash();
-    static HondaTrunk hondaTrunk = new HondaTrunk();
-    static MegaMoolah megaMoolah = new MegaMoolah();
-    static RainbowRiches rainbowRiches = new RainbowRiches();
-    static TreasureSpins treasureSpins = new TreasureSpins();
+    static DiamondDash diamondDash = new DiamondDash(); // TODO: Create a DiamondDash machine
+    static HondaTrunk hondaTrunk = new HondaTrunk(); // TODO: Create a HondaTrunk machine
+    static MegaMoolah megaMoolah = new MegaMoolah(); // TODO: Create a MegaMoolah machine
+    static RainbowRiches rainbowRiches = new RainbowRiches(); // TODO: Create a RainbowRiches machine
+    static TreasureSpins treasureSpins = new TreasureSpins(); // TODO: Create a TreasureSpins machine
 
     // Lists to manage bot threads and services
-    private static volatile boolean stopRequested = false;
-    public static List<Thread> botThreads = new ArrayList<>();
-    public static List<BotService> botServices = new ArrayList<>();
+    private static volatile boolean stopRequested = false; // TODO: This will stop the bots when it's true
+    public static List<Thread> botThreads = new ArrayList<>(); // TODO: Keep track of all the threads for bots
+    public static List<BotService> botServices = new ArrayList<>(); // TODO: Keep track of bot services
 
     /**
      * Getter that to obtain the boolean value of stopRequested
      * @return the value of stopRequested (true or false)
      */
-
     public static boolean getStopRequested() {
-        return stopRequested;
+        return stopRequested; // TODO: Return if stopping is requested
     }
 
     /**
@@ -45,11 +43,11 @@ public class SlotMachineManager {
 
         // List of bots
         List<Bot> bots = List.of(
-                Chase.getInstance(),
-                HondaBoyz.getInstance(),
-                MrBrooks.getInstance(),
-                ProfessorHuang.getInstance(),
-                AnitaMaxWynn.getInstance()
+                Chase.getInstance(), // TODO: Get an instance of Chase bot
+                HondaBoyz.getInstance(), // TODO: Get an instance of HondaBoyz bot
+                MrBrooks.getInstance(), // TODO: Get an instance of MrBrooks bot
+                ProfessorHuang.getInstance(), // TODO: Get an instance of ProfessorHuang bot
+                AnitaMaxWynn.getInstance() // TODO: Get an instance of AnitaMaxWynn bot
         );
 
         // List of slot machines
@@ -57,27 +55,27 @@ public class SlotMachineManager {
 
         // Start a service for each bot
         for (int i = 0; i < bots.size(); i++) {
-            Bot bot = bots.get(i);
-            Slot machine = slotMachines.get(i % slotMachines.size()); // Assign initial machine
-            BotService botService = new BotService(bot, machine);
+            Bot bot = bots.get(i); // TODO: Get the bot from the list
+            Slot machine = slotMachines.get(i % slotMachines.size()); // Assign initial machine, wrap around if needed
+            BotService botService = new BotService(bot, machine); // TODO: Create a BotService for the bot
 
             // Wrap botService in a thread and start it
-            Thread botThread = new Thread(botService);
-            botThread.start();
-            botThreads.add(botThread);
-            botServices.add(botService);
+            Thread botThread = new Thread(botService); // TODO: Wrap the botService in a thread
+            botThread.start(); // TODO: Start the bot thread
+            botThreads.add(botThread); // TODO: Add bot thread to the list
+            botServices.add(botService); // TODO: Add bot service to the list
 
             // Log the bot's assignment
             LOGGER.debug("Assigned {} to {}", bot.getName(), machine.getClass().getSimpleName());
 
             // Periodically trigger spins for this bot
-            Thread spinThread = getThread(botService);
-            botThreads.add(spinThread);
+            Thread spinThread = getThread(botService); // TODO: Create a spin thread for the bot
+            botThreads.add(spinThread); // TODO: Add the spin thread to the list
         }
 
         // Start a thread to rotate machines
-        Thread rotationThread = getThread(slotMachines);
-        botThreads.add(rotationThread);
+        Thread rotationThread = getThread(slotMachines); // TODO: Create a thread to rotate slot machines
+        botThreads.add(rotationThread); // TODO: Add the rotation thread to the list
     }
 
     @SuppressWarnings("BusyWait")
@@ -85,18 +83,18 @@ public class SlotMachineManager {
         Thread rotationThread = new Thread(() -> {
             try {
                 while (!stopRequested) {
-                    Thread.sleep(60000); // Rotate machines every 15 seconds
-                    if (stopRequested) break;
-                    rotateSlotMachines(slotMachines);
+                    Thread.sleep(60000); // Rotate machines every 60 seconds
+                    if (stopRequested) break; // Stop if requested
+                    rotateSlotMachines(slotMachines); // Rotate machines
                 }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                LOGGER.info("Thread has been interrupted", e);
+                LOGGER.info("Thread has been interrupted", e); // TODO: Log if thread is interrupted
             }
         });
 
-        rotationThread.start();
-        return rotationThread;
+        rotationThread.start(); // TODO: Start the rotation thread
+        return rotationThread; // TODO: Return the thread so we can add it to the list
     }
 
     @SuppressWarnings("BusyWait")
@@ -104,18 +102,18 @@ public class SlotMachineManager {
         Thread spinThread = new Thread(() -> {
             try {
                 while (!stopRequested) {
-                    Thread.sleep((long) (Math.random() * 6000 + 5000));
-                    if (stopRequested) break;
-                    botService.triggerSpin();
+                    Thread.sleep((long) (Math.random() * 6000 + 5000)); // TODO: Random delay between 5-11 seconds
+                    if (stopRequested) break; // Stop if requested
+                    botService.triggerSpin(); // TODO: Trigger the bot's spin
                 }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                LOGGER.info("Thread has been interrupted", e);
+                LOGGER.info("Thread has been interrupted", e); // TODO: Log if thread is interrupted
             }
         });
 
-        spinThread.start();
-        return spinThread;
+        spinThread.start(); // TODO: Start the spin thread
+        return spinThread; // TODO: Return the spin thread to be added to the list
     }
 
     /**
@@ -125,9 +123,9 @@ public class SlotMachineManager {
      * */
     private static void rotateSlotMachines(List<Slot> slotMachines) {
         for (int i = 0; i < botServices.size(); i++) {
-            BotService botService = botServices.get(i);
+            BotService botService = botServices.get(i); // Get the bot service
             Slot newMachine = slotMachines.get((i + 1) % slotMachines.size()); // Rotate to the next machine
-            botService.setSlotMachine(newMachine);
+            botService.setSlotMachine(newMachine); // TODO: Set new machine for the bot
             LOGGER.info("Rotated {} to {}", botService.getBot().getName(), newMachine.getClass().getSimpleName());
         }
     }
@@ -137,30 +135,30 @@ public class SlotMachineManager {
      * Signals threads to stop and interrupts them to end execution
      * */
     public static void stopAllThreads() {
-        stopRequested = true;
+        stopRequested = true; // TODO: Set stopRequested to true
 
         // Interrupt all bot threads
         for (Thread botThread : botThreads) {
-            if (botThread.isAlive()) {
+            if (botThread.isAlive()) { // TODO: Check if the bot thread is still running
                 try {
-                    botThread.interrupt();
-                    botThread.join(1000); //wait for threads to finish
+                    botThread.interrupt(); // TODO: Interrupt the bot thread
+                    botThread.join(1000); // Wait for 1 second for the thread to finish
                 } catch (InterruptedException e) {
-                    LOGGER.warn("Failed to stop thread: {}", botThread.getName(), e);
+                    LOGGER.warn("Failed to stop thread: {}", botThread.getName(), e); // TODO: Log any failure stopping a thread
                 }
             }
         }
 
-        LOGGER.info("All threads have been stopped.");
+        LOGGER.info("All threads have been stopped."); // TODO: Log that all threads have been stopped
     }
 
     /**
-     * Resets all threads to og state
-     * used for junit testing
+     * Resets all threads to their original state
+     * Used for junit testing
      */
     public static void reset() {
-        stopRequested = false;
-        botThreads.clear();
-        botServices.clear();
+        stopRequested = false; // TODO: Reset stopRequested to false
+        botThreads.clear(); // TODO: Clear the list of bot threads
+        botServices.clear(); // TODO: Clear the list of bot services
     }
 }
