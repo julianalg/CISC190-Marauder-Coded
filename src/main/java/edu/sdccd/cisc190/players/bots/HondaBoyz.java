4e4f18098868b085be5cb31e1ce250ec4e56c775
WaseeIbrahim07 <166268421+WaseeIbrahim07@users.noreply.github.com>
2024-12-07 19:35:12 -0800

    Update HondaBoyz.java

--- src/main/java/edu/sdccd/cisc190/players/bots/HondaBoyz.java
+++ src/main/java/edu/sdccd/cisc190/players/bots/HondaBoyz.java
@@ -6,15 +6,20 @@ package edu.sdccd.cisc190.players.bots;
  * Max luck and min aura = lowest chances of winning
  */
 public class HondaBoyz extends Bot {
-    private static final HondaBoyz instance = new HondaBoyz();
 
+    // TODO: Make the HondaBoyz bot a unique instance so it can only exist once.
+    private static final HondaBoyz instance = new HondaBoyz(); 
+
+    // TODO: Private constructor for HondaBoyz to prevent other parts of the program from creating new instances.
     private HondaBoyz() {
+        // TODO: Calling the parent constructor (Bot) to initialize with money, luck, and aura values.
         super("HondaBoyz", 1000, 1.0, 0.1); // Initial money, luck, and aura values
     }
 
+    // TODO: Create a public method to get the single instance of HondaBoyz.
     public static HondaBoyz getInstance() {
-        return instance;
+        // TODO: Return the single instance of HondaBoyz.
+        return instance; 
     }
-
 
 }
