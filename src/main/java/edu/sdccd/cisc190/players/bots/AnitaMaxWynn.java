8255e2ff749f0771a8cd43d5e245bcdfd451de48
WaseeIbrahim07 <166268421+WaseeIbrahim07@users.noreply.github.com>
2024-12-07 19:32:13 -0800

    Update AnitaMaxWynn.java

--- src/main/java/edu/sdccd/cisc190/players/bots/AnitaMaxWynn.java
+++ src/main/java/edu/sdccd/cisc190/players/bots/AnitaMaxWynn.java
@@ -6,12 +6,15 @@ package edu.sdccd.cisc190.players.bots;
  * High luck, low aura = decent chances of winning
  */
 public class AnitaMaxWynn extends Bot {
+    // TODO: Hee-hee! This is the one and only instance of Anita Max Wynn, so we keep it private and static
     private static final AnitaMaxWynn instance = new AnitaMaxWynn();
 
+    // TODO: Shamone! Here we create the constructor for Anita Max Wynn! It's private, baby, so no one can mess with it directly.
     private AnitaMaxWynn() {
-        super("Anita Max Wynn", 1000, 0.8, 0.3); // Initial money, luck, and aura values
+        super("Anita Max Wynn", 1000, 0.8, 0.3); // Initial money (1000), luck (0.8), and aura (0.3) values, just like MJ's rhythm
     }
 
+    // TODO: Woooo! This is how you get the one and only Anita Max Wynn from the class. No duplicates, just one smooth operator.
     public static AnitaMaxWynn getInstance() {
         return instance;
     }
